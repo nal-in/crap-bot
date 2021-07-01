791b7acacb44443334dc17257b0898e5640e1a7f
panic panic <stackoverflowpanic@gmail.com>
2021-07-02 01:22:10 +0530

    doge

--- bot.py
+++ bot.py
@@ -14,7 +14,17 @@ def get_waifu(anime_base_url):
             'waifu',
             'neko',
             'shinobu',
-            'megumin'
+            'megumin',
+            'cuddle',
+            'hug',
+            'awoo',
+            'kiss',
+            'pat',
+            'smug',
+            'bonk',
+            'blush',
+            'smile',
+            'wink'
     ]
     keyword = keywords[random.randint(0, len(keywords))]
     url = os.path.join(anime_base_url, keyword)
@@ -28,7 +38,21 @@ def get_doge(doge_type):
         url = os.path.join(cloudinary_base, "v1625167973/doge_smirk.png")
         return url
 
+    if doge_type == 'hindi':
+        url = os.path.join(cloudinary_base, "v1625168058/doge_hindi.jpg")
+        return url
 
+    if doge_type == 'gun':
+        url = os.path.join(cloudinary_base, "v1625168273/doge_gun.png")
+        return url
+
+    if doge_type == 'sitting':
+        url = os.path.join(cloudinary_base, "v1625168164/doge_sitting.png")
+        return url
+
+    if doge_type == 'quoge':
+        url = os.path.join(cloudinary_base, "v1625168322/doge_quoge.png")
+        return url
 
 @client.event
 async def on_message(message):
@@ -41,8 +65,9 @@ async def on_message(message):
         img = get_waifu(anime_base_url)
         await message.channel.send(img)
 
-    if msg == "crapdogesmirk":
-        img = get_doge('smirk')
-        await message.channel.send(img)
+    if msg[0:8] == "crapdoge":
+        doge_type = msg[8:]
+        response = get_doge(doge_type)
+        await message.channel.send(response)
 
 client.run(token)
